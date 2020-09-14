00cc1a0ae904f2e1ee22f74dab5f6265d7d09713
jintaro.tanba <hnnskrhtnbsd2600@icloud.com>
2020-09-14 21:50:46 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by HOUkoku on 2020/09/14.
+//  Copyright © 2020 jintaro.tanba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
